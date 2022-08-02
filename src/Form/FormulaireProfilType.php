33e3600436b4614718fa208d5c056e42f36a73a7
Kantan <107033745+Kantan-san@users.noreply.github.com>
2022-08-02 15:31:38 +0200

    Modification du fichier base.html.twig

--- src/Form/FormulaireProfilType.php
+++ src/Form/FormulaireProfilType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,7 +21,7 @@ class FormulaireProfilType extends AbstractType
             ->add('Pseudo', TextType::class)
             ->add('Prenom')
             ->add('Nom')
-            ->add('Telephone')
+            ->add('Telephone', NumberType::class)
             ->add('Email', EmailType::class)
             ->add('MotDePasse', PasswordType::class)
             ->add('ConfirmationMotDePasse', PasswordType::class)

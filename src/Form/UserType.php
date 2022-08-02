c0fab15d8f5a4f17a44feac394e55c400e9b7827
Carldevweb <karl.knezic@gmail.com>
2022-08-02 14:08:50 +0200

    Projet ENI_Sortie Ajout du .env.local Ajout du form pour la page d'accueil

---
+++ src/Form/UserType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Login;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('password')
+            ->add('login')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Login::class,
+        ]);
+    }
+}

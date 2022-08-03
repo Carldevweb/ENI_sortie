67a83b05a7881b651ae86540e44a922b536bd733
Kantan <107033745+Kantan-san@users.noreply.github.com>
2022-08-03 14:57:12 +0200

    - Modification du menu "Navigation" - Création d'un fichier nav.css

--- src/Form/FormulaireProfilType.php
+++ src/Form/FormulaireProfilType.php
@@ -12,6 +12,11 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
 
 class FormulaireProfilType extends AbstractType
 {
@@ -23,8 +28,39 @@ class FormulaireProfilType extends AbstractType
             ->add('Nom')
             ->add('Telephone', NumberType::class)
             ->add('Email', EmailType::class)
-            ->add('MotDePasse', PasswordType::class)
-            ->add('ConfirmationMotDePasse', PasswordType::class)
+            ->add('ConfirmationMotDePasse', RepeatedType::class, [
+                // Ajouts pour le type Repeated
+                // Doivent être placés au début
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les valeurs pour les champs mots de passe doivent être identiques.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répétez le mot de passe'],
+
+                    'mapped' => false,
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Mot de passe obligatoire',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Le mot de passe doit avoir une longueur minimum de {{ limit }} caractères.',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+
+                        new Regex(
+                            [
+                                'pattern' => '/"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$"/',
+                                'message' => 'Le mot de passe doit contenir au moins une minuscule, une majuscule, un chiffre et un caractère spécial (autorisé : @, $, !, %, *, #, ?, &)'
+                            ]
+                        )
+                    ],
+                ]
+
+            )
             ->add('Campus', ChoiceType::class,[
                 'choices' => [
                     'Paris' => 'Paris',

af4697496945103a45f5e4a6f8385e9e75bfd70d
Carldevweb <karl.knezic@gmail.com>
2022-08-04 14:39:51 +0200

    Projet ENI_Sortie Ajout du .env.local Ajout du form pour la page d'accueil + les modifications Correction de la page login en créant

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,26 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\Login;
-use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    /**
-     * @Route("/accueil", name="accueil_login")
-     */
-    public function accueil()
-    {
-        $login = new Login();
-        $utilisateurForm = $this->createForm(UserType::class, $login);
-
-        return $this->render('main/login.html.twig', [
-            'utilisateurForm' =>$utilisateurForm->createView()
-        ]);
-
-    }
     /**
      * @Route("/mdp", name="oublie_mdp")
      */

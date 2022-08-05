433a1407665cb5af0adb8e08bc226b7544331d76
mocow44 <mario.nicolas.costa@gmail.com>
2022-08-05 09:15:04 +0200

    modification des formulaires + ajout du css

--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Lieu;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,18 @@ class LieuType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
+            ->add('nom', ChoiceType::class, [
+                'label' => 'Lieu',
+                'choices' => [
+                    'Paris' => 'Paris',
+                    'Bordeaux' => 'Bordeaux',
+                    'Rennes' => 'Rennes',
+                    'Nantes' => 'Nantes',
+                    'Lyon' => 'Lyon',
+                    'Marseille' => 'Marseille'
+                ],
+                'multiple'=> false
+            ])
             ->add('rue')
             ->add('latitude')
             ->add('longitude')
--- src/Form/VilleType.php
+++ src/Form/VilleType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Ville;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,21 @@ class VilleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('codePostal')
+            ->add('nom', ChoiceType::class, [
+                'label' => 'Ville',
+                'choices' => [
+                'Paris' => 'Paris',
+                'Bordeaux' => 'Bordeaux',
+                'Rennes' => 'Rennes',
+                'Nantes' => 'Nantes',
+                'Lyon' => 'Lyon',
+                'Marseille' => 'Marseille'
+                 ],
+                'multiple'=> false
+            ])
+            ->add('codePostal', TextType::class, [
+                'label' => 'Code Postal'
+            ])
         ;
     }
 
